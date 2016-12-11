3f8e1aaa55efd547f4eeddf774fea9b0d2ccdf90
Robert Turner Jr <georob23@gmail.com>
2016-12-11 14:13:34 -0800

    Create very basic landing page.

---
+++ dist/assets/tests.js
@@ -0,0 +1,147 @@
+'use strict';
+
+define('ember-tracker/tests/app.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'app.js should pass jshint.\napp.js: line 1, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\napp.js: line 2, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\napp.js: line 3, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\napp.js: line 4, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\napp.js: line 6, col 1, \'let\' is available in ES6 (use \'esversion: 6\') or Mozilla JS extensions (use moz).\napp.js: line 13, col 3, \'object short notation\' is available in ES6 (use \'esversion: 6\') or Mozilla JS extensions (use moz).\napp.js: line 18, col 1, \'export\' is only available in ES6 (use \'esversion: 6\').\n\n7 errors');
+  });
+});
+define('ember-tracker/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = destroyApp;
+
+  function destroyApp(application) {
+    _ember['default'].run(application, 'destroy');
+  }
+});
+define('ember-tracker/tests/helpers/destroy-app.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/destroy-app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass jshint.');
+  });
+});
+define('ember-tracker/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'ember', 'ember-tracker/tests/helpers/start-app', 'ember-tracker/tests/helpers/destroy-app'], function (exports, _qunit, _ember, _emberTrackerTestsHelpersStartApp, _emberTrackerTestsHelpersDestroyApp) {
+  var Promise = _ember['default'].RSVP.Promise;
+
+  exports['default'] = function (name) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _emberTrackerTestsHelpersStartApp['default'])();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return Promise.resolve(afterEach).then(function () {
+          return (0, _emberTrackerTestsHelpersDestroyApp['default'])(_this.application);
+        });
+      }
+    });
+  };
+});
+define('ember-tracker/tests/helpers/module-for-acceptance.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/module-for-acceptance.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass jshint.');
+  });
+});
+define('ember-tracker/tests/helpers/resolver', ['exports', 'ember-tracker/resolver', 'ember-tracker/config/environment'], function (exports, _emberTrackerResolver, _emberTrackerConfigEnvironment) {
+
+  var resolver = _emberTrackerResolver['default'].create();
+
+  resolver.namespace = {
+    modulePrefix: _emberTrackerConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _emberTrackerConfigEnvironment['default'].podModulePrefix
+  };
+
+  exports['default'] = resolver;
+});
+define('ember-tracker/tests/helpers/resolver.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/resolver.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass jshint.');
+  });
+});
+define('ember-tracker/tests/helpers/start-app', ['exports', 'ember', 'ember-tracker/app', 'ember-tracker/config/environment'], function (exports, _ember, _emberTrackerApp, _emberTrackerConfigEnvironment) {
+  exports['default'] = startApp;
+
+  function startApp(attrs) {
+    var application = undefined;
+
+    // use defaults, but you can override
+    var attributes = _ember['default'].assign({}, _emberTrackerConfigEnvironment['default'].APP, attrs);
+
+    _ember['default'].run(function () {
+      application = _emberTrackerApp['default'].create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+    });
+
+    return application;
+  }
+});
+define('ember-tracker/tests/helpers/start-app.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/start-app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass jshint.');
+  });
+});
+define('ember-tracker/tests/resolver.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | resolver.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'resolver.js should pass jshint.\nresolver.js: line 1, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\nresolver.js: line 3, col 1, \'export\' is only available in ES6 (use \'esversion: 6\').\n\n2 errors');
+  });
+});
+define('ember-tracker/tests/router.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | router.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'router.js should pass jshint.\nrouter.js: line 1, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\nrouter.js: line 2, col 1, \'import\' is only available in ES6 (use \'esversion: 6\').\nrouter.js: line 4, col 1, \'const\' is available in ES6 (use \'esversion: 6\') or Mozilla JS extensions (use moz).\nrouter.js: line 12, col 1, \'export\' is only available in ES6 (use \'esversion: 6\').\n\n4 errors');
+  });
+});
+define('ember-tracker/tests/test-helper', ['exports', 'ember-tracker/tests/helpers/resolver', 'ember-qunit'], function (exports, _emberTrackerTestsHelpersResolver, _emberQunit) {
+
+  (0, _emberQunit.setResolver)(_emberTrackerTestsHelpersResolver['default']);
+});
+define('ember-tracker/tests/test-helper.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | test-helper.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass jshint.');
+  });
+});
+/* jshint ignore:start */
+
+require('ember-tracker/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+
+/* jshint ignore:end */
+//# sourceMappingURL=tests.map
